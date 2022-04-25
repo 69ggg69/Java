b04e0ddbfc6be2cf943860da552a24ea8f471c6b
69ggg69 <103955916+69ggg69@users.noreply.github.com>
2022-04-25 22:41:48 +0300

    Add files via upload

---
+++ p2/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+public class Main {
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("Вы не передавали параметров");
+        } else {
+            System.out.println("Вы ввели " + args.length + " параметров");
+        }
+    }
+}
