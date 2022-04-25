3b05d9ba925ff57b69ca709a04f32259f9ee9adc
69ggg <katerinaz443@gmail.com>
2022-04-25 22:18:10 +0300

    1

---
+++ Java 1/practice1.java
@@ -0,0 +1,10 @@
+public class practice1 {
+
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("Hello World");
+        } else {
+            System.out.println("Hello " + args[0]);
+        }
+    }
+}
